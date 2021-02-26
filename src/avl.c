0d062f309ebb5fe558dc3ee1c4f33e5ddfe0d6ac
ilitteri <ilitteri@fi.uba.ar>
2021-02-25 23:09:54 -0300

    implemented avl_create & avl_node_create

--- src/avl.c
+++ src/avl.c
@@ -7,10 +7,10 @@
 
 struct Node
 {
-    struct Node *izq;
-    struct Node *der;
-    char *clave;
-    void *dato;
+    struct Node *left;
+    struct Node *right;
+    char *key;
+    void *data;
 };
 
 struct AVL
@@ -21,7 +21,53 @@ struct AVL
     size_t count;
 };
 
-struct abb_iter
+struct AVL_Iter
 {
     Stack *states;
 };
+
+static Node *node_create(const char *key, void *data);
+
+static Node *node_create(const char *key, void *data)
+{
+    Node *node = malloc(sizeof(Node));
+
+    if (node == NULL)
+    {
+        return NULL;
+    }
+
+    char *copy = malloc(strlen(key)+1);
+
+    if (copy == NULL)
+    {
+        free(node);
+        return NULL;
+    }
+
+    strcpy(copy, key);
+
+    node->key = copy;
+    node->dato = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+AVL *avl_crear(avl_cmp_key cmp, avl_destroy_data destroy_data)
+{
+    AVL *avl;
+
+    if ((avl = malloc(sizeof(AVL))) == NULL)
+    {
+        return NULL;
+    }
+
+    avl->root = NULL;
+    avl->cmp = cmp;
+    avl->destroy_data = destroy_data;
+    avl->count = 0;
+
+    return avl;
+}
